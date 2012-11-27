6722b775f4545adc0b2e9a7af19544fd13031b6c
IFAN CHU <ifanchu@IFANs-iMac.local>
2012-11-26 17:22:39 -0800

    Initial Commit

---
+++ MiniTip/UTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UTAppDelegate.h
+//  MiniTip
+//
+//  Created by IFAN CHU on 11/26/12.
+//  Copyright (c) 2012 IFAN CHU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MiniTip/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MiniTip
+//
+//  Created by IFAN CHU on 11/26/12.
+//  Copyright (c) 2012 IFAN CHU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UTAppDelegate class]));
+    }
+}
